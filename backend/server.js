37b7e562000d461a9c684d6e1b3b3072ddf3ec03
Rafa6tz <rafa6tz@gmail.com>
2025-06-15 18:35:59 -0300

    do the user login, register and auth logic

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Server running')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
